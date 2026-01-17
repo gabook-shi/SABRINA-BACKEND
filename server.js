af1974d8ca26b52f955eb7bff4521038a040d5a9
gabook-shi <andreigab102007@gmail.com>
2026-01-17 11:01:37 +0800

    Update server.js

--- server.js
+++ server.js
@@ -2,10 +2,21 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
+// In-memory storage for basket data
+let basketData = {};
+
+// ESP32 sends data here
 app.post('/updateBasket', (req, res) => {
   const { basket_id, tag_id, weight } = req.body;
-  console.log(`Basket: ${basket_id}, Tag: ${tag_id}, Weight: ${weight}`);
+  if (!basketData[basket_id]) basketData[basket_id] = [];
+  basketData[basket_id].push({ tag_id, weight });
   res.json({ status: 'success' });
+});
+
+// WordPress fetches data here
+app.get('/getBasket/:id', (req, res) => {
+  const basket_id = req.params.id;
+  res.json(basketData[basket_id] || []);
 });
 
 app.listen(3000, () => console.log('Server running on port 3000'));
